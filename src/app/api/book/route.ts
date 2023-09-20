b9ae0b660a7b02ea4cec7bafed4989c61d7c67d1
hojunin <dlsghwns@naver.com>
2023-09-20 15:44:35 +0900

    fix: 최신순 정렬해서 가져오도록 변경

--- src/app/api/book/route.ts
+++ src/app/api/book/route.ts
@@ -9,12 +9,15 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET() {
   const connection = await handleDataBase();
   const response = await new Promise((resolve, reject) => {
-    connection.query('SELECT * FROM book', (error, result) => {
-      if (error) {
-        return reject(error);
-      }
-      return resolve(result);
-    });
+    connection.query(
+      'SELECT * FROM book ORDER BY created ASC',
+      (error, result) => {
+        if (error) {
+          return reject(error);
+        }
+        return resolve(result);
+      },
+    );
   });
 
   return NextResponse.json(response);
